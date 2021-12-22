d790415883249317398bd6c1d19e02e4bab4c740
Andres Diaz <andiazo@unal.edu.co>
2021-12-22 12:24:00 -0500

    folder structure

--- src/main.py
+++ src/main.py
@@ -1,5 +1,38 @@
+ALGORITMOS = ['ALGO1', 'ALGO2', 'ALGO3']
+
+def algo0():
+    print('Algoritmo 0')
+
+def algo1():
+    print('Algoritmo 1')
+
+def algo2():
+    print('Algoritmo 2')
+
+def intro():
+    print('UNDIRECTED GRAPHICAL MODELS')
+    for i, algo in enumerate(ALGORITMOS):
+        print(i, algo)
+    
+def algoritmos():
+    error = 'Ingrese la opción correcta'
+    
+    switch = {
+        0: algo0,
+        1: algo1,
+        2: algo2,
+        3: quit,
+    }
+
+    while True:
+        opc = int(input('Ingrese su opción: '))
+        switch.get(opc, error)()
+
+
 def main():
-    print("UNDIRECTED GRAPHICAL MODELS")
+    intro()
+    algoritmos()
+    
 
 
 if __name__=='__main__':
